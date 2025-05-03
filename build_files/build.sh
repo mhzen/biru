54e8b344ba35ce2c7da16b281680365093845615
Mhzen <warofzen1@proton.me>
2025-05-03 23:06:22 +0700

    Prep

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,11 @@
 
 set -ouex pipefail
 
-### Install packages
+# Install packages
+dnf5 install -y ulauncher
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# Fix nix on F42+
+mkdir /nix
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+# Ujust
+echo "import \"/usr/share/biru/biru.just\"" >>/usr/share/ublue-os/justfile
