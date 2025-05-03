b4566ce9b9885ccb9d6d78ce41c045bdc1be4d26
Mhzen <warofzen1@proton.me>
2025-05-03 23:50:49 +0700

    try albert

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,7 +3,8 @@
 set -ouex pipefail
 
 # Install packages
-dnf5 install -y ulauncher
+dnf5 config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:manuelschneid3r/Fedora_$(rpm -E %fedora)/home:manuelschneid3r.repo
+dnf5 install -y albert
 
 # Fix nix on F42+
 mkdir /nix
