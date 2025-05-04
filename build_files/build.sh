6d33b7d59dd7e0e95892eaddeff34bd1bc6b85fb
Mhzen <warofzen1@proton.me>
2025-05-04 12:42:12 +0700

    system flatpak test

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 # Install packages
-dnf5 config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:manuelschneid3r/Fedora_$(rpm -E %fedora)/home:manuelschneid3r.repo
+dnf5 config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:manuelschneid3r/Fedora_"$(rpm -E %fedora)"/home:manuelschneid3r.repo
 dnf5 install -y albert
 
 # Fix nix on F42+
@@ -11,3 +11,6 @@ mkdir /nix
 
 # Ujust
 echo "import \"/usr/share/biru/biru.just\"" >>/usr/share/ublue-os/justfile
+
+# Default flatpaks
+cat /usr/share/biru/system_flatpaks.list >>/usr/etc/ublue-os/system_flatpaks.list
