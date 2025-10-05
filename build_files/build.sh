8295802808aec4058ccb646485c66f7c8b43e1c0
mhzen <88493761+mhzen@users.noreply.github.com>
2025-10-05 09:38:36 +0700

    sync local changes

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,12 @@
 
 set -ouex pipefail
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# add repos
+dnf config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:manuelschneid3r/Fedora_42/home:manuelschneid3r.repo
+dnf copr enable -y alternateved/keyd
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y fish keyd albert
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+# for nix compatibility
+mkdir /nix
